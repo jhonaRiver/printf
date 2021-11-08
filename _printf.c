606a122b482be98747205b816df4756a9b153e28
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-11-08 14:52:15 +0000

    Added files task 0

---
+++ _printf.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _printf - Produces output according to a format
+ * @format: character string
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	char *s;
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			count++;
+			format++;
+			continue;
+		}
+		format++;
+		switch (*format)
+		{
+			case 'c':
+				_putchar(va_arg(args, int));
+				count++;
+				break;
+			case 's':
+				while (*format != '\0')
+				{
+					_putchar(va_arg(args, int));
+					count++;
+				}
+				break;
+		}
+	}
+	return (count);
+}
