023daf463873a19e507f7328d4840e1b149a0b13
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-11-09 15:01:15 +0000

    Modified file

--- _printf.c
+++ _printf.c
@@ -109,3 +109,15 @@ int _putnum(int num)
 	}
 	return (count);
 }
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
