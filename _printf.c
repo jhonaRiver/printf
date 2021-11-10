dc2ee42caca03aa6b279c96b5baad044d6c2b690
MenchacaS <3615@holbertonschool.com>
2021-11-10 03:05:19 +0000

    Modified file task 1

--- _printf.c
+++ _printf.c
@@ -2,7 +2,7 @@
 
 /**
  * _printf - Produces output according to a format
- * @format: character strings
+ * @format: character string
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
